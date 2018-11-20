2e074a085ac77e6bb9152717bdb8058ca62e9d96
farahait <farah.aitoumeziane@ada.ac.uk>
2018-11-20 14:37:18 +0000

    implement state functionality
    implement a function for each of the states of the game (title screen, main loop, game over screen)

--- game.js
+++ game.js
@@ -2,8 +2,17 @@ let state = 0
 // 0 = START
 // 1 = MAIN
 // 2 = STOP
-
-function setup(){
-	createCanvas(600, 600)
+function start() {
 	background(0)
+}
+
+function main() {
+	background(255)
+}
+
+function stop() {
+
+}
+function setup() {
+	createCanvas(600, 600)
 }
